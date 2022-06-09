61c3545337cb95c7a292fb96b8da10de665520b5
wachira <wachiraa954@gmail.com>
2022-06-09 21:47:34 +0300

    commit tree d483a2365c5b1d526ae1ef1c03bdd7bf66fdc259

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  
+  if(someValue <= 400){
+  return "This one is on me!";
+}
+ else if(someValue > 2000 && someValue < 2500) {
+  return "I will gladly take your thirty bucks."
+  } else {
+      return ("No can do.")
+    }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city == "NYC" ? "ok, sounds good." : "No go.");
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+    return("Thank you so much.")
+    break;
+    case "not as generous":
+    return("Thank you.")
+    break;
+    default:
+    return("Bye.")
+  }
 }
